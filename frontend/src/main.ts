7f59897177cc3d0db62e690711b3211a14d9b6b2
Shalitha Vikum <hsvikum@gmail.com>
2022-02-24 10:38:59 +0530

    💡 added vue-toast-notification to show events

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueToast from 'vue-toast-notification';
 import App from './App.vue'
 import './index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
-
+app.use(VueToast).provide('toast', app.config.globalProperties.$toast)
 app.mount('#app')
