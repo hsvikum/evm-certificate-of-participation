4c776e8d7e4254c1ec465c719e0b16d0bf230cbd
Shalitha Vikum <hsvikum@gmail.com>
2022-05-31 15:54:12 +0530

    modified the contract data for the developer training

--- contracts/CertificateOfParticipationNFT.sol
+++ contracts/CertificateOfParticipationNFT.sol
@@ -11,20 +11,20 @@ contract CertificateOfParticipationNFT is ERC721URIStorage {
     Counters.Counter private _tokenIds;
     mapping(address => uint) public mintedItem;
 
-    constructor() ERC721("Certificate Of Participation for TECHTALK EP:01", "TCOP") {
+    constructor() ERC721("Certificate Of Participation for Developer Training", "COPDT") {
     }
 
     function generateTokenURL(string memory participentName) internal pure returns (string memory){
-        string memory imageURL = 'ipfs://QmQMqz8RV4FsKD9B6BQyaPQwoE8Dpvkm9jJzAi2vtosCAR';
+        string memory imageURL = 'ipfs://QmTBTbrSSaXAqWiHuj1Lw2erWjArFvGw3ZrmCKfC8EZk3n';
         string memory empty = '';
         string memory json = '';
 
         if (keccak256(bytes(participentName)) == keccak256(bytes(empty))) {
-            json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Smart Contracts: TECHTALK EP:01", "description": "This NFT certifies that the original holder joined the Smart Contracts webinar session conducted by OrangeHRM", "image_data": "', imageURL, '"}'))));
+            json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Smart Contracts: Developer Training", "description": "This NFT certifies that the original holder joined the Smart Contracts Developer Training session conducted by OrangeHRM", "image_data": "', imageURL, '"}'))));
         } else {
             require(checkForInvalidCharacters(participentName), "Contains invalid characters");
             // Not supporting special characters. Adding special characters may corrupt your NFTs meta data
-            json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Smart Contracts: TECHTALK EP:01", "description": "This NFT certifies that ', participentName ,' joined the Smart Contracts webinar session conducted by OrangeHRM", "image_data": "', imageURL, '"}'))));
+            json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Smart Contracts: Developer Training", "description": "This NFT certifies that ', participentName ,' joined the Smart Contracts Developer Training session conducted by OrangeHRM", "image_data": "', imageURL, '"}'))));
         }
         return string(abi.encodePacked('data:application/json;base64,', json));
     }
@@ -54,8 +54,8 @@ contract CertificateOfParticipationNFT is ERC721URIStorage {
     }
 
     function getCertificate(string memory participentName) public {
-        // prevent minting after 1646915400 unix time
-        require(block.timestamp < 1646915400, 'Time to mint the NFT is over');
+        // prevent minting after 1654596348 unix time
+        require(block.timestamp < 1654596348, 'Time to mint the NFT is over');
         // prevent minting more than one nft per address
         require(mintedItem[msg.sender] == 0, 'Cannot mint more than one NFT per address');
 
